f26fb8808b4e5d6a64bc803448e19164bc7d1cec
silvershade1 <silvershade1000@gmail.com>
2024-08-05 16:59:22 +0330

    first commit

---
+++ src/script/main.js
@@ -0,0 +1,51 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+const model = './assets/scene.gltf';
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+const loader = new GLTFLoader();
+loader.load(
+  model,
+  function (gltf) {
+    scene.add(gltf.scene);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.z = 20;
+camera.position.y = 20;
+window.addEventListener("resize", () => {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  renderer.setSize(width, height);
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+});
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+animate();
